d785490616e8ae05abfeb4c968a700ef0db87736
David Mondok <3883758+davidmondok@users.noreply.github.com>
2023-11-23 10:34:14 +0100

    add class value initialization features

--- src/Config.php
+++ src/Config.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Kaiseki\Config;
 
+use Kaiseki\Config\Exception\UnkownClassNameException;
 use Psr\Container\ContainerInterface;
+
+use function is_object;
 
 final class Config
 {
@@ -15,18 +18,46 @@ final class Config
 
     /**
      * @template T of object
-     * @phpstan-param array<array-key, class-string<T>> $map
+     *
+     * @phpstan-param array<array-key, class-string<T>|T> $map
+     *
      * @phpstan-return array<array-key, T>
+     *
+     * @param ContainerInterface $container
+     * @param array              $map
      */
     public static function initClassMap(ContainerInterface $container, array $map): array
     {
         $init = [];
-        foreach ($map as $key => $className) {
+        foreach ($map as $key => $value) {
             /** @phpstan-var T $object */
-            $object = $container->get($className);
+            $object = self::initClass($container, $value);
             $init[$key] = $object;
         }
         return $init;
+    }
+
+    /**
+     * @template T of object
+     *
+     * @phpstan-param class-string<T>|T $value
+     *
+     * @phpstan-return T
+     *
+     * @param ContainerInterface $container
+     * @param object|string      $value
+     */
+    public static function initClass(ContainerInterface $container, string|object $value): object
+    {
+        if (is_object($value)) {
+            return $value;
+        }
+        if (!$container->has($value)) {
+            throw UnkownClassNameException::fromName($value);
+        }
+        /** @phpstan-var T $object */
+        $object = $container->get($value);
+        return $object;
     }
 
     /**
